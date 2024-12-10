4c53ce39bc1d9c4237920a76233bf691522206fd
cyccro <cycrohuh@gmail.com>
2024-12-10 18:40:59 -0300

    sent to windowing basic window system

--- examples/window.php
+++ examples/window.php
@@ -1,25 +1,10 @@
 <?php
 
+require_once "src/window.php";
+
 function main()
 {
-	if (!glfwInit()) throw new ErrorException("Could not initialize GLFW");
-	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	$window = glfwCreateWindow(800, 600, "Window test");
-	if (!$window) {
-		throw new ErrorException("Could not create window");
-	}
-	glfwMakeContextCurrent($window);
-	glfwSwapInterval(1);
-	while (true) {
-		glfwPollEvents();
-		glfwSwapBuffers($window);
-		if (glfwWindowShouldClose($window)) break;
-	}
+	$window = new Window(800, 600, "Made by rick");
+	$window->run();
 }
 main();
