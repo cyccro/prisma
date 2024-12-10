2438e05a9f4572b65b9a6840a482d588f30ba844
cyccro <cycrohuh@gmail.com>
2024-12-10 16:37:17 -0300

    test for checking different branch

--- examples/window.php
+++ examples/window.php
@@ -12,8 +12,6 @@ function main()
 
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	$window = glfwCreateWindow(800, 600, "Prisma test");
-	if (!$window) {
-		throw new ErrorException("Could not create window");
-	}
+	if (!$window) throw new ErrorException("Could not create window");
 }
 main();
